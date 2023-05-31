ece905b3a29a399e313136d66996e62dd253e901
khinezinthin <khinezinthinwk1@gmail.com>
2023-05-31 21:54:27 +0630

    first navbar

---
+++ src/routes/Path.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const Path = () => {
+  return (
+    <div>
+        <Navbar/>
+        
+        Path
+    </div>
+  )
+}
+
+export default Path
